8f7623390c21212c6d84808e1597325231475cd3
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-09-08 15:11:40 -0400

    Building the content.

--- sketch.js
+++ sketch.js
@@ -1,8 +1,17 @@
+var textInput;
+var textOutput;
+
 function setup() {
     noCanvas();
-    createP("Sounds in the Woods");
-    createP("Computational poetry with Jack Kerouac");
-    createP("Just a test");
+    // createP("Sounds in the Woods");
+    // createP("Computational poetry with Jack Kerouac");
+    // createP("Just a test");
+    textInput = select("#editableP");
+
+    textOutput = select("#output");
+    textInput.input(function() {
+        textOutput.html(textInput.html());
+    });
 }
 
 function draw() {
