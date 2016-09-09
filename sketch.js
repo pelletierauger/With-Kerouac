6a58086b40ed3cab6426e1cfbeffdba459409b9e
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-09-08 20:43:27 -0400

    Added text input.

--- sketch.js
+++ sketch.js
@@ -4,6 +4,10 @@ var mashed;
 
 function setup() {
     noCanvas();
+    // var fileSelect = createFileInput(gotFile);
+    // fileSelect.parent(select("#fileBox"));
+    loadStrings('sounds-in-the-woods.txt', pickString);
+
     // createP("Sounds in the Woods");
     // createP("Computational poetry with Jack Kerouac");
     // createP("Just a test");
@@ -16,12 +20,27 @@ function setup() {
     });
 }
 
+function pickString(text) {
+    text = join(text, '</br>');
+    textOutput.html(text);
+}
+
+function gotFile(file) {
+    if (file.type === 'text') {
+        console.log(file);
+        // var formattedText = join(file.data, ' ');
+        // textInput.html(formattedText);
+        textOutput.html(file.data);
+    }
+
+}
+
 function mash(text) {
     // console.log(text);
     var textOut = "";
     // console.log(text.length);
     for (var i = 0; i < text.length; i++) {
-        var currentIndex = random(text.length - 2);
+        var currentIndex = floor(random(text.length - 2));
         textOut += text.substring(currentIndex, currentIndex + 2);
         // console
         // console.log(text.substring(currentIndex, currentIndex + 1));
