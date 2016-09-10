20be93fd1a2c542803a9731befbb0b068ca9d72f
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-09-10 19:37:18 -0400

    canvasScale test

--- sketch3.js
+++ sketch3.js
@@ -60,11 +60,12 @@ function draw() {
         gradient.addColorStop(1, "rgba(50,30,0,255)");
         ctx.fillStyle = gradient;
         rect(-width / 2, -height, width, height);
-        if (windowWidth < 1000 && deviceOrientation == "portrait") {
-            scale(width / 900 * 2, width / 900 * 2);
-        } else {
-            scale(width / 900, width / 900);
-        }
+        // if (windowWidth < 1000 && deviceOrientation == "portrait") {
+        //     scale(width / 900 * 2, width / 900 * 2);
+        // } else {
+        //     scale(width / 900, width / 900);
+        // }
+
 
         recursiveTree({
             pos: createVector(0, 0),
