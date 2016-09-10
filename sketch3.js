b4acd7d14844f80b68fadb2c99cf43baf01dfbe1
Guillaume Pelletier-Auger <pelletierauger@gmail.com>
2016-09-09 21:34:09 -0400

    Fixed the background problem

--- sketch3.js
+++ sketch3.js
@@ -51,16 +51,17 @@ function mash() {
 function draw() {
     if (!mashDrawn) {
         translate(width / 2, height);
-        if (windowWidth < 1000) {
-            scale(width / 900 * 2, width / 900 * 2);
-        } else {
-            scale(width / 900, width / 900);
-        }
         var gradient = ctx.createLinearGradient(0, -height, 0, 0);
         gradient.addColorStop(0, "rgba(70,60,30,255)");
         gradient.addColorStop(1, "rgba(50,30,0,255)");
         ctx.fillStyle = gradient;
         rect(-width / 2, -height, width, height);
+        if (windowWidth < 1000) {
+            scale(width / 900 * 2, width / 900 * 2);
+        } else {
+            scale(width / 900, width / 900);
+        }
+
         recursiveTree({
             pos: createVector(0, 0),
             angle: PI / 2 * 3,
